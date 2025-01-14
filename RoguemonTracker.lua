e6f778e5f1f0d936095ad777543d134bf42a0072
something-smart <118685569+something-smart@users.noreply.github.com>
2025-01-14 12:56:15 -0500

    Increment version

--- RoguemonTracker.lua
+++ RoguemonTracker.lua
@@ -1,6 +1,6 @@
 local function RoguemonTracker()
     local self = {}
-	self.version = "0.3"
+	self.version = "0.4"
 	self.name = "Roguemon Tracker"
 	self.author = "Croz & Smart"
 	self.description = "Tracker extension for tracking & automating Roguemon rewards & caps."
