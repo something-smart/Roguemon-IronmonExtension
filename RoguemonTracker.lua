fdefe0a0a57e4e3eca82379d89fee5f82991fc19
Jason Harvey <alienth@gmail.com>
2025-06-28 14:58:36 -0800

    Release 1.3.2.

--- RoguemonTracker.lua
+++ RoguemonTracker.lua
@@ -1,6 +1,6 @@
 local function RoguemonTracker()
     local self = {}
-	self.version = "1.3.2-rc1"
+	self.version = "1.3.2"
 	self.name = "Roguemon Tracker"
 	self.author = "Croz & Smart"
 	self.description = "Tracker extension for tracking & automating Roguemon rewards & caps."
