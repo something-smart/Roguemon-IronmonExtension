b7722b2e28f964ea65fc29f83d753e2e192fe312
Jason Harvey <alienth@gmail.com>
2025-10-26 13:51:43 -0800

    Bump alpha version.

--- RoguemonTracker.lua
+++ RoguemonTracker.lua
@@ -262,7 +262,7 @@ local function RoguemonTracker()
 	-- This is the version of the ROM patch which has been bundled with the
 	-- Tracker. If the ROM is older than this, we prompt the user to patch.
 	-- This should be updated whenever `roguemon.bps` is updated.
-	local bundledRomPatchVersion = "0.4.0-alpha2"
+	local bundledRomPatchVersion = "0.4.0-alpha3"
 
 	-- This is set by the ROM. We track it to apply complementary rule enforcement in the tracker.
 	local enforceRules = false
